7ec9863a74a7a1192c6520742d55df370d9d1dd0
25lp3625 User <25lp3625@administrators-iMac.local>
2026-01-20 13:40:16 +0800

    day2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,12 +6,15 @@ import { Button } from "./compiler/Button";
 export default function Home() {
   const [value, setValue] = useState("")
   const [tasks, setTasks] = useState([])
-  const [isChecked, setIsChecked] = useState(false)
+
+  const checkedTrue = tasks.filter(task => task.checked===true)
+  const checkedFalse = tasks.filter(task => task.checked===false)
+  
 
   const Counter = () => {
-    return ( <div>
-    {isChecked ? (
-        <div className="flex w-86 justify-between border-t pt-4 border-[#E4E4E7] "><p>0 of 0 tasks completed</p> <button className="text-[#EF4444] cursor-pointer">Clear completed</button></div>
+    return (<div>
+      {tasks.length > 0 ? (
+        <div className="flex w-86 justify-between border-t pt-4 border-[#E4E4E7] "><p>{checkedTrue.length} of {tasks.length} tasks completed</p> <button onClick={() => { setTasks(checkedFalse) }} className="text-[#EF4444] cursor-pointer">Clear completed</button></div>
       ) : (
         <p>No tasks yet. Add one above!</p>
       )}
@@ -20,13 +23,11 @@ export default function Home() {
   }
 
   const clickAdd = () => {
-
     const task = {
-      id: tasks.length,
+      id: Date.now(),
       text: value,
       checked: false,
     };
-
     const newTasks = [...tasks, task]
     setTasks(newTasks)
     setValue("")
@@ -34,6 +35,21 @@ export default function Home() {
 
   const handleChange = (event) => {
     setValue(event.target.value)
+  }
+
+  const changeCheck = (taskId) => {
+    const changed = tasks.map((task) => {
+      if (task.id === taskId) { return { ...task, checked: !task.checked } } else {
+        return task
+      }
+    })
+
+    setTasks(changed)
+  }
+
+  const Delete = (taskId) => {
+    const deleted = tasks.filter((task) => task.id !== taskId)
+    setTasks(deleted)
   }
 
   return (<div className="flex justify-center items-center h-screen">
@@ -51,14 +67,20 @@ export default function Home() {
           <button onClick={clickAdd} className="h-10 bg-[#2463EB] text-white rounded-md w-15">Add</button>
         </div>
         <div className="flex mt-5 gap-1.5">
-          <Button text="All" />
-          <Button text="Active" />
-          <Button text="Completed" />
+          <Button text="All" click={() => { setTasks(tasks) }} />
+          <Button text="Active" click={() => { setTasks(checkedFalse) }} />
+          <Button text="Completed" click={() => { setTasks(checkedTrue) }} />
         </div>
         <div>
           {
             tasks.map((task) => (
-              <div className="bg-[#F9FAFB] flex items-center pl-4 gap-2.5 rounded-md mt-5 h-15.5" key={task.id}><input className="h-5 w-5" type="checkbox" checked={isChecked} onChange={(e) => setIsChecked(e.target.checked)}  />{task.text}</div>
+              <div className="bg-[#F9FAFB] justify-between flex items-center p-4 rounded-md mt-5 " key={task.id}>
+                <div className="flex gap-2.5 items-center">
+                  <input className="h-5 w-5" type="checkbox" checked={task.checked} onChange={() => changeCheck(task.id)} />
+                  <p className={`${task.checked && 'line-through'} w-full`}>{task.text}</p>
+                </div>
+                <button onClick={Delete(task.id)} className="text-[#EF4444] bg-[#FEF2F2] rounded-md px-3 h-8 hover:text-[#FEF2F2] hover:bg-[#EF4444] mr-2 cursor-pointer">Delete</button>
+              </div>
             ))
           }
         </div>
